a69b41bf7c53890fce200ce508adf746c418c319
Davi Mateus <davi.2020109493@unicap.br>
2022-01-11 10:51:05 -0300

    project created

---
+++ backend/src/test/java/com/davimateus/dmmovie/DmMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davimateus.dmmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
