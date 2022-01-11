a665b3c6936f461831049560bef4ec09ab4eddeb
Davi Mateus <davi.2020109493@unicap.br>
2022-01-11 11:48:53 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from "Pages/Listing";
+import Form from "Pages/Form";
 import Navbar from "Components/Navbar";
 
-const App = () => {
+function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Pages/Form/index.tsx
@@ -0,0 +1,9 @@
+
+const Form = () => {
+    return(
+      <h1>Página de Formulário</h1>
+    );
+  };
+  
+  export default Form;
+  
