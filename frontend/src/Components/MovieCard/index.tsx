f7c486e10ef1bb530ac3049fa7e86f4dbb9266a0
Davi Mateus <davi.2020109493@unicap.br>
2022-01-15 18:33:44 -0300

    Props

--- frontend/src/Components/MovieCard/index.tsx
+++ frontend/src/Components/MovieCard/index.tsx
@@ -1,15 +1,12 @@
 import MovieScore from "Components/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 
-const MovieCard = () => {
-  const movie = {
-    id: 1,
-    image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5,
-  };
+type Props = {
+  movie: Movie;
+}
+
+const MovieCard = ( { movie } : Props ) => {
 
   return (
     <div>
--- frontend/src/Pages/Listing/index.tsx
+++ frontend/src/Pages/Listing/index.tsx
@@ -7,39 +7,38 @@ import { BASE_URL } from "utils/requests";
 
 const Listing = () => {
   const [pageNumber, setPageNumber] = useState(0);
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
 
   useEffect(() => {
     axios
-      .get(`${BASE_URL}/movies?size=12&page=0`)
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
       .then((res) => {
         const data = res.data as MoviePage;
-        console.log(data)
-        setPageNumber(data.number);
+        setPage(data);
       })
       .catch();
-  }, []);
+  }, [pageNumber]);
 
   return (
     <>
-      <p>{pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
+          {page.content.map((movie) => (
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          ))}
         </div>
       </div>
     </>
